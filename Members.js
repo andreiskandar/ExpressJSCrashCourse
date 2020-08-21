731ee00ab510e2c27b90bedcf88ff42b6b50519a
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-20 21:28:23 -0700

    added course

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Bob Williams',
+    email: 'bobwilliams@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 2,
+    name: 'Annamarie Wunsch',
+    email: 'annamariewunsch@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 3,
+    name: 'Harvey Walsh',
+    email: 'haverywalsh@gmail.com',
+    status: 'inactive'
+  }
+];
+
+module.exports = members;
